71ee485999d23bd0390ccd5c297bda5e92fd7599
Andrey1008 <98596465+Andrey1008@users.noreply.github.com>
2026-03-18 15:41:49 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+set -e
+
 mysql -u $DATABASE_USERNAME -p $DATABASE_PASSWORD -e 'CREATE DATABASE $DATABASE_NAME;'
 
 python manage.py makemigrations
 
 python manage.py migrate
+
+exec "$@"
